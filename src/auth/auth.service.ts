ac04f631b55be93253b539defcd496bdb2af013b
akwasiasamoah <emmanuel.asamoah@hubtel.com>
2023-12-22 21:26:40 +0000

    prisma config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
     console.log(dto);
     // get email and password
     // check if user exists
+    const userExists = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
 
+    console.log(userExists);
     // hash password
     // store the user in the database
     return { status: 200, msg: 'signup function' };
